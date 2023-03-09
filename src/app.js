42c3a20c70a8c68031a20a86d485bb302f01c097
vishwesh991 <vishwesh.vispute@lntinfotech.com>
2023-03-09 12:35:17 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) => {
-    res.status(200).send("Hello worrrld")
+    res.status(200).send("Hello world")
 })
 module.exports = app;
